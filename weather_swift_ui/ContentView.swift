6e74f69319e666371e8a1f525fab06bc579d9121
siddhant <sidroxx.sc@gmail.com>
2025-10-26 20:26:41 +0530

    Initial Commit

---
+++ weather_swift_ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather_swift_ui
+//
+//  Created by Siddhant Chouhan on 26/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather_swift_ui/weather_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_swift_uiApp.swift
+//  weather_swift_ui
+//
+//  Created by Siddhant Chouhan on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct weather_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
